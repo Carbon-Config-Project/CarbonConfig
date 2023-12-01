3798abf1eebda1cc5faa99359d579b65f9f25a38
Speiger <speiger@gmx.net>
2023-12-01 09:24:52 +0100

    Removed Dead Code

--- src/main/java/carbonconfiglib/gui/widgets/CarbonEditBox.java
+++ src/main/java/carbonconfiglib/gui/widgets/CarbonEditBox.java
@@ -48,7 +48,6 @@ public class CarbonEditBox extends GuiTextField implements IOwnable, GuiResponde
 	}
 	
 	public CarbonEditBox setSuggestion(String value) {
-		//TODO implement me?
 		return this;
 	}
 	
