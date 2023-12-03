33befbabc2aa3cafd3a3c0224a67d74c0ed81113
Speiger <speiger@gmx.net>
2023-12-03 05:35:02 +0100

    Fixing that per world configs didn't load on joining a MP world

--- src/main/java/carbonconfiglib/impl/internal/CarbonConfigHooks.java
+++ src/main/java/carbonconfiglib/impl/internal/CarbonConfigHooks.java
@@ -1,15 +1,38 @@
 package carbonconfiglib.impl.internal;
 
-import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
-import net.minecraft.launchwrapper.IClassTransformer;
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.tree.*;
-
 import java.util.ListIterator;
 import java.util.Map;
 
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.FieldInsnNode;
+import org.objectweb.asm.tree.FrameNode;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.VarInsnNode;
+
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+import net.minecraft.launchwrapper.IClassTransformer;
+
+/**
+ * Copyright 2023 Speiger, Meduris
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 @IFMLLoadingPlugin.Name("CarbonConfigHooks")
 @IFMLLoadingPlugin.SortingIndex(1002)
 public class CarbonConfigHooks implements IFMLLoadingPlugin, IClassTransformer {
