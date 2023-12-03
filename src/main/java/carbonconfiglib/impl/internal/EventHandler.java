3c5b24bb4d5d80139a5d576c6b2d1d5a20f7928f
Speiger <speiger@gmx.net>
2023-12-03 05:31:51 +0100

    Fixing that per world configs didn't load on joining a MP world

--- src/main/java/carbonconfiglib/impl/internal/EventHandler.java
+++ src/main/java/carbonconfiglib/impl/internal/EventHandler.java
@@ -189,7 +189,7 @@ public class EventHandler implements IConfigChangeListener
 	@SubscribeEvent
 	@OnlyIn(Dist.CLIENT)
 	public void onPlayerServerJoinEvent(LoggingIn event) {
-		if(Minecraft.getInstance().getCurrentServer() != null) loadMPConfigs();
+		if(Minecraft.getInstance().getCurrentServer() == null) loadMPConfigs();
 		CarbonConfig.NETWORK.sendToServer(new StateSyncPacket(Dist.CLIENT));
 		BulkSyncPacket packet = BulkSyncPacket.create(CarbonConfig.CONFIGS.getConfigsToSync(), SyncType.CLIENT_TO_SERVER, true);
 		if(packet == null) return;
@@ -200,7 +200,7 @@ public class EventHandler implements IConfigChangeListener
 	@OnlyIn(Dist.CLIENT)
 	public void onPlayerServerLeaveEvent(LoggingOut event) {
 		CarbonConfig.NETWORK.onPlayerLeft(event.getPlayer(), false);
-		if(Minecraft.getInstance().getCurrentServer() != null) {
+		if(!Minecraft.getInstance().isLocalServer()) {
 			for(ConfigHandler handler : CarbonConfig.CONFIGS.getAllConfigs()) {
 				if(PerWorldProxy.isProxy(handler.getProxy())) {
 					handler.unload();
