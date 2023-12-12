049ac52e42505b78ae7fb271a6b77eb7c6f661df
Speiger <speiger@gmx.net>
2023-12-12 22:15:18 +0100

    Fixes for GT so their configs work... All 84 of them

--- src/main/java/carbonconfiglib/impl/internal/EventHandler.java
+++ src/main/java/carbonconfiglib/impl/internal/EventHandler.java
@@ -194,7 +194,7 @@ public class EventHandler implements IConfigChangeListener
 		}
 	}
 	
-	public void processIMCEvents(Map<String, ModContainer> config) {
+	public void processIMCEvents(Map<String, ModContainer> config, Map<ModContainer, ModContainer> remapping) {
 		if(FMLCommonHandler.instance().getSide().isServer()) {
 			foundFiles.clear();
 			return;
@@ -205,6 +205,11 @@ public class EventHandler implements IConfigChangeListener
 			forgeConfigs.supplyIfAbsent(container, ObjectArrayList::new).add(C);
 		});
 		foundFiles.clear();
+		for(Map.Entry<ModContainer, ModContainer> entry : remapping.entrySet()) {
+			List<Configuration> configs = forgeConfigs.remove(entry.getKey());
+			if(configs == null || configs.isEmpty()) continue;
+			forgeConfigs.supplyIfAbsent(entry.getValue(), ObjectArrayList::new).addAll(configs);
+		}
 	}
 	
 	@SideOnly(Side.CLIENT)
