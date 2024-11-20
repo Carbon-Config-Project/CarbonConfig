139d069a60875206dd80772f71be1dedfb56c1ca
Carlos Alberto Teixeira <61806914+CarlosATeixeira@users.noreply.github.com>
2024-11-20 03:06:05 -0300

    Update ForgeDataType.java to be Typeless

--- src/main/java/carbonconfiglib/gui/impl/forge/ForgeDataType.java
+++ src/main/java/carbonconfiglib/gui/impl/forge/ForgeDataType.java
@@ -30,124 +30,160 @@ import net.minecraftforge.common.ForgeConfigSpec.ValueSpec;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-public class ForgeDataType<T>
+public class ForgeDataType
 {
-	private static final Map<Class<?>, ForgeDataType<?>> DATA_TYPES = Object2ObjectMaps.synchronize(new Object2ObjectOpenHashMap<>());
-	public static final ForgeDataType<Boolean> BOOLEAN = new ForgeDataType<>(Boolean.class, DataType.BOOLEAN, ForgeHelpers::parseBoolean, T -> T.toString(), null);
-	public static final ForgeDataType<Integer> INTEGER = new ForgeDataType<>(Integer.class, DataType.INTEGER, Helpers::parseInt, T -> T.toString(), ForgeHelpers::getIntLimit);
-	public static final ForgeDataType<Long> LONG = new ForgeDataType<>(Long.class, DataType.INTEGER, ForgeHelpers::parseLong, T -> T.toString(), ForgeHelpers::getLongLimit);
-	public static final ForgeDataType<Float> FLOAT = new ForgeDataType<>(Float.class, DataType.DOUBLE, ForgeHelpers::parseFloat, T -> T.toString(), ForgeHelpers::getFloatLimit);
-	public static final ForgeDataType<Double> DOUBLE = new ForgeDataType<>(Double.class, DataType.DOUBLE, Helpers::parseDouble, T -> T.toString(), ForgeHelpers::getDoubleLimit);
-	public static final ForgeDataType<String> STRING = new ForgeDataType<>(String.class, DataType.STRING, ForgeHelpers::parseString, Function.identity(), null);
-	
-	DataType type;
-	Function<String, ParseResult<T>> parse;
-	Function<T, String> serialize;
-	Function<Object[], String> rangeInfo;
-	
-	ForgeDataType(Class<T> clz, DataType type, Function<String, ParseResult<T>> parse, Function<T, String> serialize, Function<Object[], String> rangeInfo) {
-		this(type, parse, serialize, rangeInfo);
-		DATA_TYPES.putIfAbsent(clz, this);
-	}
-	
-	public ForgeDataType(DataType type, Function<String, ParseResult<T>> parse, Function<T, String> serialize, Function<Object[], String> rangeInfo) {
-		this.type = type;
-		this.parse = parse;
-		this.serialize = serialize;
-		this.rangeInfo = rangeInfo;
-	}
-	
-	public DataType getDataType() {
-		return type;
-	}
-	
-	public boolean isEnum() {
-		return false;
-	}
-	
-	public String getLimitations(ValueSpec spec) {
-		if(rangeInfo == null) return "";
-		Object[] data = ForgeHelpers.getRangeInfo(spec);
-		return data == null ? "" : rangeInfo.apply(ForgeHelpers.getRangeInfo(spec));
-	}
-	
-	public ParseResult<T> parse(String input) {
-		return parse.apply(input);
-	}
-	
-	public String serialize(T value) {
-		try {
-			return serialize.apply(value);
-		} catch (ClassCastException e) {
-			if (value instanceof Integer) {
-				value = (T) Long.valueOf((Integer) value);
-			} else {
-				throw e;
-			}
-			return serialize.apply(value);
-		}
-	}
-
-
-	
-	public static void registerDataType(Class<?> clz, ForgeDataType<?> type) {
-		DATA_TYPES.putIfAbsent(clz, type);
-	}
-	
-	@SuppressWarnings("unchecked")
-	public static <T> ForgeDataType<T> getDataByType(Class<T> clz) {
-		if(clz == null) return null;
-		ForgeDataType<T> type = (ForgeDataType<T>)DATA_TYPES.get(clz);
-		return type == null && clz.isEnum() ? EnumDataType.create(clz) : type;
-	}
-	
-	public static class EnumDataType<T extends Enum<T>> extends ForgeDataType<T> {
-		Class<T> clz;
-		
-		EnumDataType(Class<T> clz) {
-			super(clz, DataType.ENUM, null, null, null);
-			this.clz = clz;
-		}
-		
-		@Override
-		public boolean isEnum() { return true; }
-		
-		@Override
-		public ParseResult<T> parse(String input) {
-			try { return ParseResult.success(Enum.valueOf(clz, input)); }
-			catch (Exception e) { return ParseResult.error(input, e, "Value must be one of the following: "+Arrays.toString(toArray(null))); }
-		}
-		
-		@Override
-		public String getLimitations(ValueSpec spec) {
-			return "Value must be one of the following: "+Arrays.toString(toArray(spec));
-		}
-		
-		public List<Suggestion> getSuggestions(ValueSpec spec) {
-			List<Suggestion> result = new ObjectArrayList<>();
-			ISuggestionProvider.enums(clz).provideSuggestions(result::add, T -> spec.test(T.getValue()));
-			return result;
-		}
-		
-		@Override
-		public String serialize(T value) {
-			return value.name();
-		}
-		
-		@SuppressWarnings({"rawtypes", "unchecked" })
-		public static <T> ForgeDataType<T> create(Class<T> clz) {
-			return new EnumDataType(clz);
-		}
-		
-		private String[] toArray(ValueSpec spec) {
-			T[] array = clz.getEnumConstants();
-			List<String> values = new ObjectArrayList<>();
-			for(int i = 0,m=array.length;i<m;i++) {
-				if(spec == null || spec.test(array[i])) {
-					values.add(array[i].name());
-				}
-			}
-			return values.toArray(new String[values.size()]);
-		}
-	}
+    private static final Map<Class<?>, ForgeDataType> DATA_TYPES = Object2ObjectMaps.synchronize(new Object2ObjectOpenHashMap<>());
+    
+    // Ajuste nas funções de parse com casting
+    public static final ForgeDataType BOOLEAN = new ForgeDataType(
+        Boolean.class, 
+        DataType.BOOLEAN, 
+        input -> (ParseResult<Object>) (ParseResult<?>) ForgeHelpers.parseBoolean(input), 
+        Object::toString, 
+        null
+    );
+    
+    public static final ForgeDataType INTEGER = new ForgeDataType(
+        Integer.class, 
+        DataType.INTEGER, 
+        input -> (ParseResult<Object>) (ParseResult<?>) Helpers.parseInt(input), 
+        Object::toString, 
+        ForgeHelpers::getIntLimit
+    );
+    
+    public static final ForgeDataType LONG = new ForgeDataType(
+        Long.class, 
+        DataType.INTEGER, 
+        input -> (ParseResult<Object>) (ParseResult<?>) ForgeHelpers.parseLong(input), 
+        Object::toString, 
+        ForgeHelpers::getLongLimit
+    );
+    
+    public static final ForgeDataType FLOAT = new ForgeDataType(
+        Float.class, 
+        DataType.DOUBLE, 
+        input -> (ParseResult<Object>) (ParseResult<?>) ForgeHelpers.parseFloat(input), 
+        Object::toString, 
+        ForgeHelpers::getFloatLimit
+    );
+    
+    public static final ForgeDataType DOUBLE = new ForgeDataType(
+        Double.class, 
+        DataType.DOUBLE, 
+        input -> (ParseResult<Object>) (ParseResult<?>) Helpers.parseDouble(input), 
+        Object::toString, 
+        ForgeHelpers::getDoubleLimit
+    );
+    
+    public static final ForgeDataType STRING = new ForgeDataType(
+        String.class, 
+        DataType.STRING, 
+        input -> (ParseResult<Object>) (ParseResult<?>) ForgeHelpers.parseString(input), 
+        obj -> (String)obj,  // Substituição de Function.identity()
+        null
+    );
+    
+    DataType type;
+    Function<String, ParseResult<Object>> parse;
+    Function<Object, String> serialize;
+    Function<Object[], String> rangeInfo;
+    
+    @SuppressWarnings("unchecked")
+    ForgeDataType(Class<?> clz, DataType type, Function<String, ParseResult<Object>> parse, Function<Object, String> serialize, Function<Object[], String> rangeInfo) {
+        this(type, parse, serialize, rangeInfo);
+        DATA_TYPES.putIfAbsent(clz, this);
+    }
+    
+    public ForgeDataType(DataType type, Function<String, ParseResult<Object>> parse, Function<Object, String> serialize, Function<Object[], String> rangeInfo) {
+        this.type = type;
+        this.parse = parse;
+        this.serialize = serialize;
+        this.rangeInfo = rangeInfo;
+    }
+    
+    public DataType getDataType() {
+        return type;
+    }
+    
+    public boolean isEnum() {
+        return false;
+    }
+    
+    public String getLimitations(ValueSpec spec) {
+        if(rangeInfo == null) return "";
+        Object[] data = ForgeHelpers.getRangeInfo(spec);
+        return data == null ? "" : rangeInfo.apply(data);
+    }
+    
+    public ParseResult<Object> parse(String input) {
+        return parse.apply(input);
+    }
+    
+    public String serialize(Object value) {
+        return serialize.apply(value);
+    }
+    
+    public static void registerDataType(Class<?> clz, ForgeDataType type) {
+        DATA_TYPES.putIfAbsent(clz, type);
+    }
+    
+    public static ForgeDataType getDataByType(Class<?> clz) {
+        if(clz == null) return null;
+        ForgeDataType type = DATA_TYPES.get(clz);
+        return type == null && clz.isEnum() ? EnumDataType.create(clz) : type;
+    }
+    
+    public static class EnumDataType extends ForgeDataType {
+        Class<? extends Enum> clz;
+        
+        EnumDataType(Class<? extends Enum> clz) {
+            super(clz, DataType.ENUM, null, null, null);
+            this.clz = clz;
+        }
+        
+        @Override
+        public boolean isEnum() { return true; }
+        
+        @Override
+        public ParseResult<Object> parse(String input) {
+            try { 
+                return ParseResult.success(Enum.valueOf(clz, input)); 
+            }
+            catch (Exception e) { 
+                return ParseResult.error(input, e, "Value must be one of the following: "+Arrays.toString(toArray(null))); 
+            }
+        }
+        
+        @Override
+        public String getLimitations(ValueSpec spec) {
+            return "Value must be one of the following: "+Arrays.toString(toArray(spec));
+        }
+        
+        public List<Suggestion> getSuggestions(ValueSpec spec) {
+            List<Suggestion> result = new ObjectArrayList<>();
+            ISuggestionProvider.enums(clz).provideSuggestions(result::add, enumConstant -> spec.test(enumConstant.getValue()));
+            return result;
+        }
+        
+        @Override
+        public String serialize(Object value) {
+            return ((Enum<?>) value).name();
+        }
+        
+        @SuppressWarnings({"rawtypes", "unchecked" })
+        public static ForgeDataType create(Class<?> clz) {
+            return new EnumDataType(clz.asSubclass(Enum.class));
+        }
+        
+        private String[] toArray(ValueSpec spec) {
+            Enum[] array = clz.getEnumConstants();
+            List<String> values = new ObjectArrayList<>();
+            for(int i = 0, m = array.length; i < m; i++) {
+                if(spec == null || spec.test(array[i])) {
+                    values.add(array[i].name());
+                }
+            }
+            return values.toArray(new String[values.size()]);
+        }
+    }
 }
