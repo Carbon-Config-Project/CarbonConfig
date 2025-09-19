1dd4f8138c31b30612fe315478aa42383751d341
Speiger <speiger@gmx.net>
2025-09-20 01:49:47 +0200

    Ported Latest Changes

--- src/main/java/carbonconfiglib/impl/internal/EventHandler.java
+++ src/main/java/carbonconfiglib/impl/internal/EventHandler.java
@@ -6,31 +6,20 @@ import java.util.Map;
 import carbonconfiglib.CarbonConfig;
 import carbonconfiglib.api.IConfigChangeListener;
 import carbonconfiglib.config.ConfigHandler;
-import carbonconfiglib.gui.api.DataType;
 import carbonconfiglib.gui.api.IModConfigs;
-import carbonconfiglib.gui.api.ISuggestionRenderer;
-import carbonconfiglib.gui.config.ColorElement;
-import carbonconfiglib.gui.config.RegistryElement;
 import carbonconfiglib.gui.impl.forge.ForgeConfigs;
 import carbonconfiglib.gui.impl.minecraft.MinecraftConfigs;
 import carbonconfiglib.gui.screen.ConfigSelectorScreen;
-import carbonconfiglib.gui.widgets.SuggestionRenderers;
 import carbonconfiglib.impl.PerWorldProxy;
-import carbonconfiglib.impl.entries.ColorValue;
-import carbonconfiglib.impl.entries.ColorValue.ColorWrapper;
 import carbonconfiglib.networking.carbon.StateSyncPacket;
 import carbonconfiglib.networking.snyc.BulkSyncPacket;
 import carbonconfiglib.networking.snyc.SyncPacket;
+import carbonconfiglib.plugins.ICarbonPlugin;
 import carbonconfiglib.utils.SyncType;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.ConfigScreenHandler.ConfigScreenFactory;
@@ -73,7 +62,7 @@ public class EventHandler implements IConfigChangeListener
 	
 	@Override
 	public void onConfigCreated(ConfigHandler config) {
-		initMinecraftDataTypes(config);
+		InternalFeatures.initMinecraftDataTypes(config);
 		if(FMLEnvironment.dist.isDedicatedServer()) return;
 		ModLoadingContext context = ModLoadingContext.get();
 		if("minecraft".equals(context.getActiveNamespace())) {
@@ -81,12 +70,6 @@ public class EventHandler implements IConfigChangeListener
 			throw new IllegalStateException("Mod Configs Must be created (not loaded) during a Mod Loading Phase");
 		}
 		configs.computeIfAbsent(context.getActiveContainer(), ModConfigs::new).addConfig(config);
-	}
-	
-	public void initMinecraftDataTypes(ConfigHandler config) {
-		config.addParser('C', ColorValue::parse);
-		config.addTempParser('R');
-		config.addTempParser('K');
 	}
 	
 	@Override
@@ -129,7 +112,7 @@ public class EventHandler implements IConfigChangeListener
 	
 	@OnlyIn(Dist.CLIENT)
 	public void onConfigsLoaded() {
-		loadDefaultTypes();
+		InternalFeatures.loadDefaultTypes();
 		Object2ObjectMap<ModContainer, List<IModConfigs>> mappedConfigs = new Object2ObjectLinkedOpenHashMap<>();
 		configs.forEach((M, C) -> {
 			if(M.getCustomExtension(ConfigScreenFactory.class).isPresent()) return;
@@ -137,6 +120,7 @@ public class EventHandler implements IConfigChangeListener
 		});
 		if(CarbonConfig.FORGE_SUPPORT.get()) {
 			ModList.get().forEachModInOrder(T-> {
+				if(CarbonConfig.MODS_DISABLED.contains(T.getModId())) return;
 				if(T.getCustomExtension(ConfigScreenFactory.class).isEmpty()) {
 					ForgeConfigs configs = new ForgeConfigs(T);
 					if(configs.hasConfigs()) {
@@ -148,24 +132,13 @@ public class EventHandler implements IConfigChangeListener
 				};
 			});
 		}
-		mappedConfigs.forEach((M, C) -> M.registerExtensionPoint(ConfigScreenFactory.class, () -> new ConfigScreenFactory((U, S) -> create(S, ModConfigList.createMultiIfApplicable(M, C)))));
-	}
-	
-	@OnlyIn(Dist.CLIENT)
-	private void loadDefaultTypes() {
-		ISuggestionRenderer.Registry.register(Item.class, new SuggestionRenderers.ItemEntry());
-		ISuggestionRenderer.Registry.register(Block.class, new SuggestionRenderers.ItemEntry());
-		ISuggestionRenderer.Registry.register(Fluid.class, new SuggestionRenderers.FluidEntry());
-		ISuggestionRenderer.Registry.register(Enchantment.class, new SuggestionRenderers.EnchantmentEntry());
-		ISuggestionRenderer.Registry.register(ColorWrapper.class, new SuggestionRenderers.ColorEntry());
-		ISuggestionRenderer.Registry.register(MobEffect.class, new SuggestionRenderers.PotionEntry());
+		ICarbonPlugin.LOADED_PLUGINS.forEach((K, V) -> {
+			List<IModConfigs> configs = new ObjectArrayList<>();
+			V.applyConfigs(K, configs::add);
+			if(configs.size() > 0) mappedConfigs.computeIfAbsent(K, T -> new ObjectArrayList<>()).addAll(configs);
+		});
 		
-		DataType.registerType(Item.class, RegistryElement.createForType(Item.class, "minecraft:air"));
-		DataType.registerType(Block.class, RegistryElement.createForType(Block.class, "minecraft:air"));
-		DataType.registerType(Fluid.class, RegistryElement.createForType(Fluid.class, "minecraft:empty"));
-		DataType.registerType(Enchantment.class, RegistryElement.createForType(Enchantment.class, "minecraft:fortune"));
-		DataType.registerType(MobEffect.class, RegistryElement.createForType(MobEffect.class, "minecraft:luck"));
-		DataType.registerType(ColorWrapper.class, new DataType(false, "0xFFFFFFFF", ColorElement::new, ColorElement::new, ColorElement::new));
+		mappedConfigs.forEach((M, C) -> M.registerExtensionPoint(ConfigScreenFactory.class, () -> new ConfigScreenFactory((U, S) -> create(S, ModConfigList.createMultiIfApplicable(M, C)))));
 	}
 	
 	@OnlyIn(Dist.CLIENT)
