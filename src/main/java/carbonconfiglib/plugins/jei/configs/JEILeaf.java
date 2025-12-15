a2a625daca4337744e8bb172d02fe902475d9b7f
Speiger <speiger@gmx.net>
2025-12-15 16:58:36 +0100

    Small Fix to make Curseforge release work properly as the last seem broken.

--- src/main/java/carbonconfiglib/plugins/jei/configs/JEILeaf.java
+++ src/main/java/carbonconfiglib/plugins/jei/configs/JEILeaf.java
@@ -103,7 +103,7 @@ public class JEILeaf implements IConfigNode
 	public Component getTooltip() {
 		MutableComponent comp = Component.empty();
 		comp.append(entry.getLocalizedName().copy().withStyle(ChatFormatting.YELLOW)).append("\n");
-		if(entry.getLocalizedDescription() != null) comp.append(entry.getLocalizedDescription()).append("\n");
+		if(entry.getLocalizedDescription() != null) comp.append(entry.getLocalizedDescription().copy().withStyle(ChatFormatting.GRAY)).append("\n");
 		IJeiConfigValueSerializer<?> serializer = isArray ? ((IJeiConfigListValueSerializer<?>)this.serializer).getListValueSerializer() : this.serializer;
 		comp.append(Component.literal(serializer.getValidValuesDescription()).withStyle(ChatFormatting.BLUE));
 		return comp;
