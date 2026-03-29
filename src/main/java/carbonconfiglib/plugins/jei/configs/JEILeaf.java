c9cc9ba96e71d9fa10f03d74b443fa17f76d5072
Speiger <speiger@gmx.net>
2026-03-29 21:17:05 +0200

    Last Minute Fixes

--- src/main/java/carbonconfiglib/plugins/jei/configs/JEILeaf.java
+++ src/main/java/carbonconfiglib/plugins/jei/configs/JEILeaf.java
@@ -150,7 +150,6 @@ public class JEILeaf implements IConfigNode
 	@Override
 	public Component getTooltip() {
 		MutableComponent comp = Component.empty();
-		comp.append(entry.getLocalizedName().copy().withStyle(ChatFormatting.YELLOW)).append("\n");
 		if(entry.getLocalizedDescription() != null) comp.append(entry.getLocalizedDescription().copy().withStyle(ChatFormatting.GRAY)).append("\n");
 		IJeiConfigValueSerializer<?> serializer = isArray ? ((IJeiConfigListValueSerializer<?>)this.serializer).getListValueSerializer() : this.serializer;
 		comp.append(Component.literal(serializer.getValidValuesDescription()).withStyle(ChatFormatting.BLUE));
