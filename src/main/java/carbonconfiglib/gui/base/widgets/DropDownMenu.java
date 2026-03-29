2ba98e28bea4888ab86f5334cc92ffe93ff04ad0
Speiger <speiger@gmx.net>
2026-03-29 21:13:16 +0200

    Custom Renderer fix

--- src/main/java/carbonconfiglib/gui/base/widgets/DropDownMenu.java
+++ src/main/java/carbonconfiglib/gui/base/widgets/DropDownMenu.java
@@ -225,6 +225,7 @@ public class DropDownMenu<T> extends CarbonButton {
 				owner.renderSelection(left, top, width, height, GuiUtils.brighter(getSelectionColor(false), 0.8F), getSelectionBackgroundColor());
 			}
 			if(renderer != null) {
+				renderer.setOwner(owner);
 				renderer.render(x, top+1, left+1, width-2, height-2, mouseX, mouseY, selected, partialTicks);
 				return;
 			}
