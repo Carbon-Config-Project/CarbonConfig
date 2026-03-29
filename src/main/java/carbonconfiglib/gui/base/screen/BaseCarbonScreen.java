78ab1c66f4fdee1c76bdccbb85aef67a7124f764
Speiger <speiger@gmx.net>
2026-03-29 21:15:57 +0200

    Last Minute Fixes

--- src/main/java/carbonconfiglib/gui/base/screen/BaseCarbonScreen.java
+++ src/main/java/carbonconfiglib/gui/base/screen/BaseCarbonScreen.java
@@ -53,6 +53,7 @@ public class BaseCarbonScreen extends Screen
 {
 	public static final int DEFAULT_DELAY = 200;
 	protected List<Widget> renderables = new ObjectArrayList<>();
+	protected boolean renderBackground = true;
 	protected int centerX;
 	protected int centerY;
 	protected int tick;
@@ -95,6 +96,7 @@ public class BaseCarbonScreen extends Screen
 	
 	@Override
 	public void render(PoseStack matrix, int mouseX, int mouseY, float partialTicks) {
+		if(renderBackground) renderBackground(matrix);
 		renderBackground(matrix, mouseX, mouseY, partialTicks);
 		renderWidgets(matrix, mouseX, mouseY, partialTicks);
 		renderForeground(matrix, mouseX, mouseY, partialTicks);
