08f1d8a6f271eaa87238c9523e0869a8a532d476
Speiger <speiger@gmx.net>
2026-03-29 21:16:04 +0200

    Last Minute Fixes

--- src/main/java/carbonconfiglib/gui/base/screen/BaseCarbonScreen.java
+++ src/main/java/carbonconfiglib/gui/base/screen/BaseCarbonScreen.java
@@ -51,6 +51,7 @@ import speiger.src.collections.objects.lists.ObjectArrayList;
 public class BaseCarbonScreen extends Screen
 {
 	public static final int DEFAULT_DELAY = 200;
+	protected boolean renderBackground = true;
 	protected int centerX;
 	protected int centerY;
 	protected int tick;
@@ -85,6 +86,7 @@ public class BaseCarbonScreen extends Screen
 	
 	@Override
 	public void render(PoseStack matrix, int mouseX, int mouseY, float partialTicks) {
+		if(renderBackground) renderBackground(matrix);
 		renderBackground(matrix, mouseX, mouseY, partialTicks);
 		renderWidgets(matrix, mouseX, mouseY, partialTicks);
 		renderForeground(matrix, mouseX, mouseY, partialTicks);
--- src/main/java/carbonconfiglib/gui/base/widgets/DropDownMenu.java
+++ src/main/java/carbonconfiglib/gui/base/widgets/DropDownMenu.java
@@ -112,6 +112,7 @@ public class DropDownMenu<T> extends CarbonButton {
 			this.owner = owner;
 			this.ownerState = owner.getState();
 			generateElements();
+			renderBackground = false;
 		}
 		
 		private void generateElements() {
@@ -224,6 +225,7 @@ public class DropDownMenu<T> extends CarbonButton {
 				owner.renderSelection(poseStack, left, top, width, height, GuiUtils.brighter(getSelectionColor(false), 0.8F), getSelectionBackgroundColor());
 			}
 			if(renderer != null) {
+				renderer.setOwner(owner);
 				renderer.render(poseStack, x, top+1, left+1, width-2, height-2, mouseX, mouseY, selected, partialTicks);
 				return;
 			}
