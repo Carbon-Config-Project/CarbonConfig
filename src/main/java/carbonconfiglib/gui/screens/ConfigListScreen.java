bcd9031faef018b2388482eaba0d76225e55065d
Speiger <speiger@gmx.net>
2026-04-15 17:42:26 +0200

    Ported to 1.12.2

--- src/main/java/carbonconfiglib/gui/screens/ConfigListScreen.java
+++ src/main/java/carbonconfiglib/gui/screens/ConfigListScreen.java
@@ -23,6 +23,7 @@ import carbonconfiglib.gui.base.widgets.CarbonList.ListState;
 import carbonconfiglib.impl.internal.BackupManager;
 import carbonconfiglib.impl.internal.BackupManager.BulkRequest;
 import carbonconfiglib.impl.internal.BackupManager.Mode;
+import carbonconfiglib.impl.internal.BackupManager.SingleRequest;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.toasts.SystemToast;
@@ -255,6 +256,13 @@ public class ConfigListScreen extends BaseCarbonScreen
 		
 		private void updateBackup() {
 			if(shouldCreatePick()) return;
+			Minecraft mc = Minecraft.getMinecraft();
+			if(isInWorldConfig() && !mc.isSingleplayer()) {
+				new SingleRequest(config, T -> {
+					this.hasBackups = OptionalInt.of(BackupManager.listBackups(T).size());					
+				});
+				return;
+			}
 			this.hasBackups = OptionalInt.of(BackupManager.listBackups(config).size());
 		}
 		
@@ -362,6 +370,11 @@ public class ConfigListScreen extends BaseCarbonScreen
 		}
 		
 		private void selectBackups() {
+			Minecraft mc = Minecraft.getMinecraft();
+			if(isInWorldConfig() && !mc.isSingleplayer()) {
+				pendingRequest = new BulkRequest(ObjectLists.singleton(config), Mode.LIST);
+				return;
+			}
 			setExternalScreen(new BackupSelectionScreen(Minecraft.getMinecraft().currentScreen, holder, config, BackupManager.listBackups(config)));
 		}
 		
