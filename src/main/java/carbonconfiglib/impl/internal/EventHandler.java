36fb658810a0cf4dd852c7a380cd0557441848dd
Speiger <speiger@gmx.net>
2026-05-01 02:26:40 +0200

    Small Hotfix

--- src/main/java/carbonconfiglib/impl/internal/EventHandler.java
+++ src/main/java/carbonconfiglib/impl/internal/EventHandler.java
@@ -83,6 +83,7 @@ public class EventHandler implements IConfigChangeListener
 	@Override
 	public void onConfigChanged(ConfigHandler config) {
 		if(FMLEnvironment.dist.isDedicatedServer()) {
+			if(ServerLifecycleHooks.getCurrentServer() == null) return;
 			SyncPacket packet = SyncPacket.create(config, SyncType.SERVER_TO_CLIENT, false);
 			if(packet != null) CarbonConfig.NETWORK.sendToAllPlayers(packet);
 			return;
